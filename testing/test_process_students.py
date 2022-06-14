083fba4f9069f20bf360663daaa4932af83857fd
Roman Downie <romandownie@mines.edu>
2022-06-14 09:49:57 -0600

    added some test cases

--- testing/test_process_students.py
+++ testing/test_process_students.py
@@ -10,13 +10,30 @@ def test_get_classes_taken_by_cwid():
     
     # course data
         # Create Course row and add to database
-    testUser1 = User(
-                id = 00000000)
-    new_course = Course(
-                department = 'TEST', course_number=000, course_name="test course name", credit_hours = 0.0, is_taken="False", user_id = testUser1.id)
-    db.session.add(new_course)
-    db.session.commit()
+    #testUser1 = User(
+    #            id = 00000000)
+    #new_course = Course(
+    #            department = 'TEST', course_number=000, course_name="test course name", credit_hours = 0.0, is_taken="False", user_id = testUser1.id)
+    #db.session.add(new_course)
+    #db.session.commit()
 
-    class_list = process_students.get_classes_taken_by_cwid(12345678)
-    print(class_list)
-    assert(False)
+    #class_list = process_students.get_classes_taken_by_cwid(12345678)
+    #print(class_list)
+    #assert(False)
+    pass
+
+def test_get_major_by_cwid():
+    # testing testUser1
+    cwid = 10101010
+    major = process_students.get_major_by_cwid(cwid)
+    assert major == "TEST MAJOR1"
+
+    # testing testUser1
+    cwid = 20202020
+    major = process_students.get_major_by_cwid(cwid)
+    assert major == "TEST MAJOR2"
+
+    # testing testUser1
+    cwid = 30303030
+    major = process_students.get_major_by_cwid(cwid)
+    assert major == "TEST MAJOR3"
